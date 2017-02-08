986deece744c390b7ecb45487da40925b13a7253
Rosie <r.gollancz@gmail.com>
2017-02-08 16:47:27 +0000

    Adds quality tests

--- Java/src/main/java/com/gildedrose/Brie.java
+++ Java/src/main/java/com/gildedrose/Brie.java
@@ -3,5 +3,40 @@ package com.gildedrose;
 /**
  * Created by rosie on 08/02/2017.
  */
+
 public class Brie {
+
+    private String name;
+
+    private int sellIn;
+
+    private int quality;
+
+    public Brie(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getSellIn() {
+        return this.sellIn;
+    }
+
+    public int getQuality() {
+        return this.quality;
+    }
+
+    public Brie update() {
+        this.sellIn --;
+        updateQuality();
+        return this;
+    }
+
+    private void updateQuality() {
+        if (this.quality < 50) this.quality ++;
+    }
 }
--- Java/src/test/java/com/gildedrose/BrieTest.java
+++ Java/src/test/java/com/gildedrose/BrieTest.java
@@ -1,3 +1,8 @@
+package com.gildedrose;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -5,4 +10,44 @@ import static org.junit.Assert.*;
  */
 public class BrieTest {
 
+    private Brie brie, brieMaxQuality;
+
+    @Before
+    public void setUp() {
+        brie = new Brie("Brie", 10, 8);
+        brieMaxQuality = new Brie("Brie", 0, 50);
+    }
+
+    @Test
+    public void getName() {
+        assertEquals(brie.getName(), "Brie" );
+    }
+
+    @Test
+    public void getSellIn() {
+        assertEquals(brie.getSellIn(), 10 );
+    }
+
+    @Test
+    public void getQuality() {
+        assertEquals(brie.getQuality(), 8 );
+    }
+
+    @Test
+    public void updateReducesSellIn() {
+        brie.update();
+        assertEquals(brie.getSellIn(), 9);
+    }
+
+    @Test
+    public void updateIncreasesQuality() {
+        brieMaxQuality.update();
+        assertEquals(brieMaxQuality.getQuality(), 50);
+    }
+
+    @Test
+    public void maximumQuality50() {
+        brie.update();
+        assertEquals(brie.getQuality(), 9);
+    }
 }
